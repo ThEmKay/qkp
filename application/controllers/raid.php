8795e82656b1046d4036f5d358a5e7d57b4f4ad5
Seb <sebkoine@gmail.com>
2018-10-11 14:21:26 +0200

    Beta 0.4

--- application/controllers/raid.php
+++ application/controllers/raid.php
@@ -1,7 +1,7 @@
 <?php
     defined('BASEPATH') OR exit('No direct script access allowed');
     
-    class raid extends CI_Controller{
+    class Raid extends CI_Controller{
     
         public function index(){
           
