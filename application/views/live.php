ca0bfc9fc5d85809f9d88885a77870205ddecd60
Seb <sebkoine@gmail.com>
2018-10-22 16:31:44 +0200

    Item Tooltip Test

--- application/views/live.php
+++ application/views/live.php
@@ -9,6 +9,23 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
         <script src="http://code.jquery.com/ui/1.10.0/jquery-ui.js"></script>
+        
+                <script>
+        
+
+                $(document).ready(function(){
+                
+                           
+          
+          $.get( "https://classicdb.ch?item=14555&power", function( data ) {
+              alert( "Load was performed." );
+            });
+            
+                });
+                
+        
+        </script>
+        
         <style>
         html { 
               background: url(<?php echo base_url(); ?>img/World-of-Warcraft-Art.jpg) no-repeat center center fixed; 
@@ -50,8 +67,10 @@
               width: 20px
             }
                               
-        </style>   
-             
+        </style>         
+        
+        
+        <a href="https://classicdb.ch?item=14555&power">asdasdsd</a>
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
