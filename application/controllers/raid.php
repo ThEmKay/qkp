66b3bd01ee63ea4fd813d3e06058b13dcc463e30
Seb <sebkoine@gmail.com>
2018-11-08 16:16:25 +0100

    Queryanpassung 2

--- application/controllers/raid.php
+++ application/controllers/raid.php
@@ -176,7 +176,7 @@
                   $this->db->insert('konten', array('name' => $this->input->post('txtKontoNeu'),
                                                    'kurz' => $this->input->post('txtKontoNeuKurz')));
                                                    
-                  $this->db->query("INSERT INTO bonus (konto, spieler, wert) SELECT ".$this->input->post('txtKontoNeuKurz')." AS konto, name, 0 AS wert FROM spieler WHERE parent IS NULL;");
+                  $this->db->query("INSERT INTO bonus (konto, spieler, wert) SELECT \"".$this->input->post('txtKontoNeuKurz')."\" AS konto, name, 0 AS wert FROM spieler WHERE parent IS NULL;");
 
                   if ($this->db->trans_status() === FALSE){
                           echo "fehler";
